b8829594661a11d7730ed5eb6a5965017ce6a24c
João Vitor <43214611+vitorjoaom@users.noreply.github.com>
2023-04-10 19:21:58 -0300

    Main
    Arquivo principal do projeto, contendo o array sortido da escala de cada voluntário.

---
+++ EscalaHorarios/Escala.js
@@ -0,0 +1,14 @@
+function escala(sortescala){
+    let dias = [];
+    let pessoas = []
+    
+    while(pessoas == "fim"){
+    console.read(pessoas[i]);
+    }
+    for(i = 0; i<size; i++){
+        for(j = 1; j<size;j+=2){
+            dias[i] = pessoas[j]; 
+            dias[i+1] = pessoas[j-i];       
+        }
+    }
+}
